a029488825c4d2aaecf99266490e0dc8644542a1
Yosi Golan <yosi@seevoov.com>
2017-11-19 17:24:47 +0200

    remove frontpage module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,35 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {AppBrowserRoutingModule} from './app.routing.module';
-import {FrontPageModule} from './frontpage/frontpage.module';
-import {AboutComponent} from './about/about.component';
-import {UserProfileComponent} from './user-profile/user-profile.component';
-import {CoreModule} from './core.module';
-import {ProductComponent} from './product/product.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app.routing.module';
+import { AboutComponent } from './about/about.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { CoreModule } from './core.module';
+import { ProductComponent } from './product/product.component';
+import { DesktopFrontpageComponent } from './frontpage/desktop/desktop-frontpage.component';
+import { MobileFrontpageComponent } from './frontpage/mobile/mobile-frontpage.component';
 
 @NgModule({
-  declarations: [
+  declarations:    [
     AppComponent,
     AboutComponent,
     UserProfileComponent,
-    ProductComponent
+    ProductComponent,
+    DesktopFrontpageComponent,
+    MobileFrontpageComponent
   ],
-  imports: [
+  imports:         [
     BrowserModule,
     BrowserAnimationsModule,
     CoreModule,
-    AppBrowserRoutingModule,
-    FrontPageModule
+    AppRoutingModule
   ],
   entryComponents: [
-    UserProfileComponent
+    UserProfileComponent,
+    MobileFrontpageComponent
   ],
-  bootstrap: [
+  bootstrap:       [
     AppComponent
   ]
 })
