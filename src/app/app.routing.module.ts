6b86ca49295adb2624ecb288aedca27262dddc04
Yosi Golan <yosi@seevoov.com>
2017-11-20 16:36:59 +0200

    add routes injector

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -43,6 +43,43 @@ export class AppRoutingModule {
       router.resetConfig(mobile_routes);
     }
   }
+
+  /**
+   * this function inject new routes for the given module instead the current routes. the operation happens on the given current routes object so after
+   * this method a call to reset routes on router should be called with the the current routes object.
+   * @param currentRoutes
+   * @param routesToInject
+   * @param childNameToReplaceRoutesUnder - the module name to replace its routes.
+   */
+  private injectModuleRoutes(currentRoutes: Routes, routesToInject: Routes, childNameToReplaceRoutesUnder: string): void {
+    for (let i = 0; i < currentRoutes.length; i++) {
+      if (currentRoutes[i].loadChildren != null &&
+        currentRoutes[i].loadChildren.toString().indexOf(childNameToReplaceRoutesUnder) != -1) {
+        // we found it. taking the route prefix
+        let prefixRoute: string = currentRoutes[i].path;
+        // first removing the module line
+        currentRoutes.splice(i, 1);
+        // now injecting the new routes
+        // we need to add the prefix route first
+        this.addPrefixToRoutes(routesToInject, prefixRoute);
+        for (let route of routesToInject) {
+          currentRoutes.push(route);
+        }
+        // since we found it we can break the injection
+        return;
+      }
+
+      if (currentRoutes[i].children != null) {
+        this.injectModuleRoutes(currentRoutes[i].children, routesToInject, childNameToReplaceRoutesUnder);
+      }
+    }
+  }
+
+  private addPrefixToRoutes(routes: Routes, prefix: string) {
+    for (let i = 0; i < routes.length; i++) {
+      routes[i].path = prefix + '/' + routes[i].path;
+    }
+  }
 }
 
 
