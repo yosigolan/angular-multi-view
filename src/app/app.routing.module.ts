7061eb8e727df79fdc7e61bb32bb33c5452b2092
Yosi Golan <yosi@seevoov.com>
2018-06-20 17:28:39 +0300

    add products route

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,31 +1,31 @@
-import {NgModule} from '@angular/core';
-import {PreloadAllModules, Router, RouterModule, Routes} from '@angular/router';
-import {DesktopFrontpageComponent} from './frontpage/desktop/desktop-frontpage.component';
-import {AboutComponent} from './about/about.component';
-import {MobileFrontpageComponent} from './frontpage/mobile/mobile-frontpage.component';
-import {UserProfileComponent} from './user-profile/user-profile.component';
-import {ProductComponent} from './product/product.component';
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, Router, RouterModule, Routes } from '@angular/router';
+import { DesktopFrontpageComponent } from './frontpage/desktop/desktop-frontpage.component';
+import { AboutComponent } from './about/about.component';
+import { MobileFrontpageComponent } from './frontpage/mobile/mobile-frontpage.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { ProductComponent } from './product/product.component';
 import { ApplicationStateService } from './application-state.service';
 import { ProductComponentDesktop } from './product/product.component.desktop';
 import { ProductComponentMobile } from './product/product.component.mobile';
 
 const desktop_routes: Routes = [
-  {path: 'about', component: AboutComponent},
   {
     path: '', component: DesktopFrontpageComponent, children:
-    [
-      {path: ':productName', component: ProductComponentDesktop}
-    ]
+          [
+            {path: 'products/:productName', component: ProductComponentDesktop}
+          ]
   },
+  {path: 'about', component: AboutComponent},
   // directs all other routes to the main page
   {path: '**', redirectTo: ''}
 ];
 
 const mobile_routes: Routes = [
   {path: '', component: MobileFrontpageComponent},
+  {path: 'products/:productName', component: ProductComponentMobile},
   {path: 'about', component: AboutComponent},
   {path: 'user-profile', component: UserProfileComponent},
-  {path: ':productName', component: ProductComponentMobile},
   // directs all other routes to the main page
   {path: '**', redirectTo: ''}
 ];
